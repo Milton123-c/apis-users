409532715619e8e1f9a21c14b32512a7a13a8532
Milton123-c <miltonmercado92@gmail.com>
2023-06-01 10:54:40 -0600

    version 0.1 dialect

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,6 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL,{
+    host: 'localhost',
+    dialect: 'postgres'
+})
 
 module.exports = sequelize;
