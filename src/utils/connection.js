1b4427eb2d0db851427ad3f2ade7efb252669a64
Milton123-c <miltonmercado92@gmail.com>
2023-06-01 13:36:25 -0600

    version 0.9

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -2,11 +2,8 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  "users_sfme",
-  "users_sfme_user",
-  "6rSHo6weWthxxSGd4wC1i4uB54k7Ci7E",
+  process.env.DATABASE_URL,
   {
-    host: 'localhost',
     dialect: "postgres",
   }
 );
